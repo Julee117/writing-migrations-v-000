384644af7a6061c184f9a7902cea4a3d2a6433ec
Julianne Kim <juliannne@gmail.com>
2017-11-09 21:34:54 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,6 +2,8 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :name
+      t.integer :grade
+      t.string :birthdate
     end
   end
 end
